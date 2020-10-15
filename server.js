f606ca08c294e31efab638738ba652d7b5413d72
Glitch (hello-express) <none>
2020-10-15 02:25:36 +0000

    🎋🙆 Checkpoint ./server.js:23816504/930

--- server.js
+++ server.js
@@ -6,7 +6,27 @@
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+ // response.sendFile(__dirname + "/views/index.html");
+response.send("speech-to-txt")
+});
+app.get("/download", (req, res) => {
+
+
+var Speaker = require('speaker');
+var OpenJTalk = require('node-openjtalk').OpenJTalk;
+
+// pre-included HTS voice file
+var fn_voice = OpenJTalk.voices.mei_normal;
+// instantiate OpenJTalk with an HTS voice
+var open_jtalk = new OpenJTalk({voice: fn_voice});
+
+// synthesize a voice buffer from a text
+open_jtalk.synthesize(req.query.text, function(error, buffer) {
+  if (!error) {
+    // save as audio file
+    buffer.pipe(res)
+  }
+});
 });
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
